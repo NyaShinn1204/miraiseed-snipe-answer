fba8df65c5bb9f659187d450f74fb899042fb305
NyaShinn1204 <134133778+NyaShinn1204@users.noreply.github.com>
2024-05-13 22:35:30 +0900

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1,13 +1,26 @@
-// はい。これは今やっている画面のやつしか取得できません
+// はい。これは今やっている画面のやつしか取得できません。
 // はい。これは国語で使えません。
-// はい。これは自動的に進めません
+// はい。これは自動的に進めません。
+// はい。これは自動入力ができません。
 function getCurrentQuestion() {
   var current = $('.question-container.current');
   var currentId = current.attr('id').split('-');
   
   var answer_html = current.html().match(/【(?:答え|解答)】(?:.|\n)*?(?=<|$)/g);
-    
-  return answer_html;
-}
+  
+  var answer_text = answer_html.map(function(html) {
+    return html.replace(/【(?:答え|解答)】/, '').trim();
+  });
 
-console.log(getCurrentQuestion());
+  $('.answer-input-choices-container dd').filter(function() {
+    return $(this).find('span').text().trim() === answer_text[0];
+  }).trigger("click");
+
+  $('.choice-enter-button').trigger("click")
+  setTimeout(function(){}, Math.random()*1200)
+  $('.description-pane-next-icon next').trigger("click")
+
+      
+  return answer_text;
+}
+getCurrentQuestion();
